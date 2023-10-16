ac29529e899688ad46b304cc7a9fac69e70e194b
azizelo <33883774+azizelo@users.noreply.github.com>
2023-10-16 10:24:24 -0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,5 @@
+ 'myuser': {
+            'aws_access_key_id': 'AKIAXUQ34KE64G7ZZAWY',
+            'aws_secret_access_key': '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',
+            'region': 'myuser-region',
+        },
